7b0733d8dd977fdac77e43c6b7b355a39c8def23
Marcin <marcinszymaszek@wp.pl>
2022-12-26 16:47:33 +0100

    Added final version 3

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -45,7 +45,7 @@ export default {
     border-radius: 5px;
     &:focus-visible {
       outline: none;
-      border-color: $color-blue-light;
+      border-color: $color-purple;
     }
     @include breakpoint(medium) {
       flex-basis: 55%;
@@ -61,7 +61,7 @@ p {
 }
 
 a {
-  color: $color-primary;
+  color: $color-purple;
   text-decoration: none;
 }
 </style>
